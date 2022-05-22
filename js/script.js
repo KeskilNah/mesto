d3e47e189a2da4ecda9619202485cb8caa732ef9
Keskil Nikolaev <nik96keskil@yandex.ru>
2022-05-22 15:33:51 +0700

    исправлены названия переменных, сделан универсальная функция закрытия открытия попапов, файл с массивом вынесен в отдельный файл

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-const popup = document.querySelector('.popup');
-const edit = document.querySelector('.profile__edit');
-const closeEdit = document.querySelector('.popup__exit');
-const nameInput = document.querySelector('.popup__text_name');
-const jobInput = document.querySelector('.popup__text_description');
-const saveButton = document.querySelector('.popup__save-button');
+const editPopup = document.querySelector('.edit-popup');
+const btnEdit = document.querySelector('.profile__edit');
+const closeEdit = document.querySelector('.edit-popup__exit');
+const editForm = document.querySelector('.edit-popup__form');
+const nameInput = document.querySelector('.edit-popup__text_name');
+const jobInput = document.querySelector('.edit-popup__text_description');
+const saveButton = document.querySelector('.edit-popup__save-button');
 const nameOutput = document.querySelector('.profile__title');
-const DescriptionOutput = document.querySelector('.profile__subtitle');
+const descriptionOutput = document.querySelector('.profile__subtitle');
 
 const addingPopup = document.querySelector('.adding-popup');
 const add = document.querySelector('.profile__button');
@@ -21,141 +22,106 @@ const imgPopup = document.querySelector('.image-popup');
 const imgExit = document.querySelector('.image-popup__exit')
 const imgPopupPic = document.querySelector('.image-popup__pic');
 const imgPopupTitle = document.querySelector('.image-popup__title');
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-]; 
-
-
-
-
+const itemTemplate = document.querySelector('.item-template').content.querySelector('.item');
 
 //переворачиваем наш массив, что бы при добавлении нового элемента он, добавлялся в начало
 initialCards.reverse();
 
-//функция создания новых блоков
+//функция функцию создания карточки
 function makeGallery (item) {
+  const itemCell = itemTemplate.cloneNode(true);
   const cardName = item.name;
   const cardLink =  item.link;
-  const itemTemplate = document.querySelector('.item-template').content.querySelector('.item').cloneNode(true);
-  const btnDeletItem = itemTemplate.querySelector('.item__delete');
-  const imgItem = itemTemplate.querySelector('.item__image');
-  const textItem = itemTemplate.querySelector('.item__text');
-  const btnLikeItem = itemTemplate.querySelector('.item__like');
-  btnDeletItem.addEventListener('click', btnDeleteFun);
+  const btnDeletItem = itemCell.querySelector('.item__delete');
+  const imgItem = itemCell.querySelector('.item__image');
+  const textItem = itemCell.querySelector('.item__text');
+  const btnLikeItem = itemCell.querySelector('.item__like');
+  btnDeletItem.addEventListener('click', handleDeleteCard);
   btnLikeItem.addEventListener('click', btnLikeFun);
   imgItem.addEventListener('click', popupImg);
   textItem.textContent = cardName;
   imgItem.src = cardLink;
   imgItem.alt = cardName;
-  startList.prepend(itemTemplate);
-  console.log(itemTemplate);
+  startList.prepend(itemCell);
 };
 
-//вызов функции создания элемента для каждого элемента массива initialCards
-initialCards.forEach(makeGallery);
+//функция добавления карточки в разметку из массива initialCards
+function renderCard () {
+  initialCards.forEach(makeGallery);
+}
+
+//вызов функции добавления карточки в разметку из массива initialCards
+renderCard ()
 
 //функция добавляения нового элемента из заданых значений
 function addItem(evt) {
   evt.preventDefault();
   makeGallery(({link: urlInput.value, name: placeInput.value}));
-  console.log(initialCards);
-  closeAdd();
+  closePopup (addingPopup);
 }
 
 //слушатель добавляения нового элемента
-addSaveButton.addEventListener('click', addItem);
+addingForm.addEventListener('submit', addItem);
 
 //функция удаления элемента
-function btnDeleteFun (evt) {
+function handleDeleteCard (evt) {
   evt.target.closest('.item').remove();
 }
 
-//функция открытия попап окна с изображением, на которое мы кликнули
-function popupImg (evt) {
-  console.log(evt.target.src);
-  console.log(evt.target.textContent);
-  imgPopup.classList.toggle('image-popup_opend');
-  imgPopupPic.src = evt.target.src;
-  imgPopupTitle.textContent = evt.target.alt;
+function popupImg (imgLink) {
+  const imgSrc = imgLink.target.getAttribute('src');
+  const imgName = imgLink.target.getAttribute('alt');
+  imgPopupPic.src = imgSrc;
+  imgPopupPic.alt = imgName;
+  imgPopupTitle.textContent = imgName;
+  openPopup (imgPopup);
 }
 
-//функция закрытия попап окна с изображением
-function closeImg () {
-  imgPopup.classList.toggle('image-popup_opend');
+//универсальная функция открытия попапа
+function openPopup(popup) {
+  popup.classList.toggle('popup_opend');
+}
+//универсальная функция закрытия попапа
+function closePopup (popup) {
+  popup.classList.remove('popup_opend');
 }
 
 //слушатель закрытия попап окна с изображением
-imgExit.addEventListener('click', closeImg)
+imgExit.addEventListener('click', function(evt) {
+  closePopup(imgPopup);
+})
 
 //функция изменения изображения при клике на лайк
 function btnLikeFun (evt) {
   evt.target.classList.toggle('item__like_active');
 }
 
-//функция открытия попап окна с добавлением элемента
-function showAdd() {
-  addingPopup.classList.add('adding-popup_opened');
-}
 //слушатель открытия попап окна с добавлением элемента при нажатии на плюсик
-add.addEventListener('click', showAdd);
-
-//функция закрытия попап окна с добавлением элемента
-function closeAdd() {
-  addingPopup.classList.remove('adding-popup_opened');
-}
+add.addEventListener('click', function(evt){
+  openPopup (addingPopup);
+});
 
 //слушатель закрытия попап окна с добавлением элемента при нажатии на плюсик
-exitAdd.addEventListener('click', closeAdd);
-
-
-
-//функция открытия попап окна с изменением имени и описания
-function showPopup() {
-  popup.classList.add('popup_opened')
-}
+exitAdd.addEventListener('click', function(evt){
+  closePopup (addingPopup);
+});
 
 //слушатель открытия попап окна с изменением имени и описания при нажатии на карандашик
-edit.addEventListener('click', showPopup);
-
-//функция закрытия попап окна с изменением имени и описания
-function closePopup() {
-  popup.classList.remove('popup_opened')
-}
+btnEdit.addEventListener('click', function(evt){
+  openPopup (editPopup);
+});
 
 //слушатель закрытия попап окна с изменением имени и описания при нажатии на крестик
-closeEdit.addEventListener('click', closePopup);
+closeEdit.addEventListener('click', function(evt){
+  closePopup (editPopup);
+});
 
 //функция изменения имени и описания
 function changeName() {
   nameOutput.textContent = `${nameInput.value}`;
-  DescriptionOutput.textContent = `${jobInput.value}`;
-  closePopup()
+  descriptionOutput.textContent = `${jobInput.value}`;
+  closePopup (editPopup);
 }
 
 //слушатель кнопки сохранения попап окна с изменением имени и описания
-saveButton.addEventListener('click', changeName);
-
+editForm.addEventListener('submit', changeName);
